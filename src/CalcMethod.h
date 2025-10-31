5086587e54ffd29aef6944d4262437a76aadfe31
dpet9941 <deyan.petev@gmail.com>
2025-10-31 02:59:21 +0200

    added documentation for ParticleContainer and CalcMethod

--- src/CalcMethod.h
+++ src/CalcMethod.h
@@ -1,14 +1,27 @@
+/**
+ * @file CalcMethod.h
+ *
+ */
+
 #pragma once
 
 #define EPSILON 1e-12  // TODO: We could move this to a 'defines.h' header
 
 #include "ParticleContainer.h"
-
+/**
+ * @class CalcMethod
+ * @brief Virtual class used as a base for different calculation methods for simulation
+ *
+ */
 class CalcMethod {
  protected:
   ParticleContainer& particles;
   // TODO: Maybe move constructor and destructor to protected?
  public:
+/**
+ * @brief Constructor
+ * @param particles ParticleContainer storing the particles used by the calculation method
+ */
   explicit CalcMethod(ParticleContainer& particles) : particles(particles) {}
   virtual ~CalcMethod();
 
@@ -18,6 +31,9 @@ class CalcMethod {
       const double dt) = 0;  // TODO: for now we don't need dt, maybe remove?
 };
 
+/**
+ * @brief Stormer-Verlet calculation method
+ */
 class StormerVerletMethod : public CalcMethod {
  public:
   using CalcMethod::CalcMethod;
