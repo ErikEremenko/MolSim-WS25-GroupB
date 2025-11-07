2798f68f3ce88014795a981f84768dbc0aa9e48b
Erik Eremenko <erik.eremenko@tum.de>
2025-11-07 15:11:51 +0100

    Small improvements in testing and Cmake, build script

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -3,5 +3,5 @@ set -x
 rm -rf build/
 mkdir build && cd build
 cmake -DBUILD_DOC=ON -DENABLE_VTK_OUTPUT=ON -DVTK_DIR=/usr/local/vtk/lib/cmake/vtk-9.5 ..
-make -j 6
+cmake --build .
 # ./MolSim ../input/eingabe-sonne.txt 1000 0.014
