77712f06fa71ec0a1780a28b9cf19b48b044aca3
asdf <dpet@asd.com>
2025-11-08 16:44:53 +0100

    added FileCuboidReaderClass for reading cuboids and a temporary particle generator code segment and added example cuboid input file

---
+++ src/FileCuboidReader.h
@@ -0,0 +1,19 @@
+//
+// Created by User on 08/11/2025.
+//
+
+
+#pragma once
+
+#include <vector>
+
+#include "ParticleContainer.h"
+
+class FileCuboidReader {
+public:
+  FileCuboidReader();
+  virtual ~FileCuboidReader();
+
+  static void readFile(ParticleContainer& particles, char* filename);
+};
+
