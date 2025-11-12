85ed0f3d0bfdd1a8dd400ce14b058fdf386497b3
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2025-11-12 18:55:33 +0100

    Move Simulation class method definitions to a separate source file

--- src/Simulation.h
+++ src/Simulation.h
@@ -6,6 +6,7 @@
 #include "outputWriter/VTKWriter.h"
 
 class Simulation {
+private:
   char* filename;
   const double end_time;
   const double dt;
@@ -13,50 +14,13 @@ class Simulation {
   ParticleContainer& particles;
   ForceCalc& calcMethod;
 
-  void plotParticles(const int iteration) const {
-    const std::string out_name("MD_vtk");
-    outputWriter::VTKWriter::plotParticles(particles, out_name, iteration);
-  }
+  void plotParticles(const int iteration) const;
 
- public:
+public:
   Simulation(char* filename, const double end_time, const double dt,
-             ParticleContainer& particles, ForceCalc& calcMethod)
-      : filename(filename),
-        end_time(end_time),
-        dt(dt),
-        particles(particles),
-        calcMethod(calcMethod) {}
-  ~Simulation() = default;
+             ParticleContainer& particles, ForceCalc& calcMethod);
+  ~Simulation();
 
-  void loadParticles() const {
-    FileCuboidReader::readFile(particles, filename);
-  }
-
-  void run() const {
-    constexpr double start_time = 0;
-
-    // Load particles from file
-
-    double current_time = start_time;
-    int iteration = 0;
-
-    // for this loop, we assume: current x, current f and current v are known
-    while (current_time < end_time) {
-      // calculate new x
-      calcMethod.calculateX(dt);
-      for (auto& p : particles) {
-        p.setOldF(p.getF());  // store f(t_n) for v update
-      }
-      // calculate new f
-      calcMethod.calculateF();
-      // calculate new v
-      calcMethod.calculateV(dt);
-
-      iteration++;
-      if (iteration % 10 == 0) {
-        plotParticles(iteration);
-      }
-      current_time += dt;
-    }
-  }
+  void loadParticles() const;
+  void run() const;
 };
