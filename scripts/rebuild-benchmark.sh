77675daefc1fdf310caec88064dc917e7919046c
Erik Eremenko <erik.eremenko@tum.de>
2025-11-13 19:17:56 +0100

    Improved benchmark script and fixed ForceCalcTest

--- scripts/rebuild-benchmark.sh
+++ scripts/rebuild-benchmark.sh
@@ -3,7 +3,6 @@ set -x
 rm -rf build/
 # shellcheck disable=SC2164
 mkdir build && cd build
-cmake -DBUILD_DOC=ON -DENABLE_VTK_OUTPUT=ON -DVTK_DIR=/usr/local/vtk/lib/cmake/vtk-9.5 ..
+cmake -DBUILD_DOC=OFF -DENABLE_VTK_OUTPUT=ON -DVTK_DIR=/usr/local/vtk/lib/cmake/vtk-9.5 ..
 make -j 6
-make doc_doxygen
-./MolSim ../input/eingabe-collision.txt 1 0.0002 benchmark
+./MolSim ../input/eingabe-collision.txt 5 0.0002 benchmark
