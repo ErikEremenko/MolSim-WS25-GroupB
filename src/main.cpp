467d0923b670d6e5a315455381cebc1aa9568926
asdf <dpet@asd.com>
2025-11-13 20:22:15 +0100

    added another command line option for defining the log level

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int main(const int argc, char* argsv[]) {
   SPDLOG_INFO("Hello from MolSim for PSE!");
   // Read arguments from the command line
-  if (argc != 5) {
+  if (argc != 6) {
     SPDLOG_ERROR("Erroneous programme call!");
     SPDLOG_ERROR("./MolSim filename t_end delta_t [file | benchmark]");
     return 1;
@@ -25,6 +25,23 @@ int main(const int argc, char* argsv[]) {
     SPDLOG_ERROR("Please use 'file' or 'benchmark'");
     return 1;
   }
+
+  std::string log_level = argsv[5];
+
+  if (log_level == "info"){
+    spdlog::set_level(spdlog::level::info);
+  }else if(log_level == "off"){
+    spdlog::set_level(spdlog::level::off);
+  }else if(log_level == "trace"){
+    spdlog::set_level(spdlog::level::trace);
+  }else if(log_level == "error"){
+    spdlog::set_level(spdlog::level::err);
+  }else if(log_level == "debug"){
+    spdlog::set_level(spdlog::level::debug);
+  }else{
+    return 1;
+  }
+
   CollisionSimulation simulation(argsv[1], std::stod(argsv[2]), std::stod(argsv[3]), simulation_mode);
   simulation.run();
 
