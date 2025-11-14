a488bd0c199e83c1d578ed64ac3ef5aeb2dc46d3
Erik Eremenko <erik.eremenko@tum.de>
2025-11-14 01:02:56 +0100

    Fixed Doxygen generation

--- scripts/rebuild-benchmark.sh
+++ scripts/rebuild-benchmark.sh
@@ -6,7 +6,7 @@ rm -rf build/
 # shellcheck disable=SC2164
 mkdir build && cd build
 
-cmake -DBUILD_DOC=OFF -DENABLE_VTK_OUTPUT=ON -DVTK_DIR=/usr/local/vtk/lib/cmake/vtk-9.5 ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DOC=OFF -DENABLE_VTK_OUTPUT=ON -DVTK_DIR=/usr/local/vtk/lib/cmake/vtk-9.5 ..
 make -j 6
 # sets CPUs to maximum available frequency
 sudo cpupower frequency-set -g performance
