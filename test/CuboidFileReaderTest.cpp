3dee382b3684c4f16385efc013b7c73dc69bfb6f
ErikEremenko <74866259+ErikEremenko@users.noreply.github.com>
2025-11-29 17:31:23 +0000

    style: Apply clang-format

--- test/CuboidFileReaderTest.cpp
+++ test/CuboidFileReaderTest.cpp
@@ -6,7 +6,7 @@
 #include "io/FileReader.h"
 #include "utils/ArrayUtils.h"
 class CuboidFileReaderTest : public ::testing::Test {
-protected:
+ protected:
   std::string project_dir = PROJ_SRC_DIR;
   std::string inputFilename = project_dir + "/input/cuboid-test.txt";
   ParticleContainer pc;
@@ -16,6 +16,8 @@ protected:
 TEST_F(CuboidFileReaderTest, BasicRead) {
   CuboidFileReader reader(inputFilename);
   reader.readFile(pc);
-  EXPECT_EQ(pc[0].toString(), "Particle: X:[0, 0, 0] v: [-0.171411, 0.0178057, 0] f: [0, 0, 0] old_f: [0, 0, 0] type: 0");
-  EXPECT_EQ(pc[1].toString(), "Particle: X:[0, 1.1225, 0] v: [0.00571789, -0.14098, 0] f: [0, 0, 0] old_f: [0, 0, 0] type: 0");
+  EXPECT_EQ(pc[0].toString(),
+            "Particle: X:[0, 0, 0] v: [-0.171411, 0.0178057, 0] f: [0, 0, 0] old_f: [0, 0, 0] type: 0");
+  EXPECT_EQ(pc[1].toString(),
+            "Particle: X:[0, 1.1225, 0] v: [0.00571789, -0.14098, 0] f: [0, 0, 0] old_f: [0, 0, 0] type: 0");
 }
