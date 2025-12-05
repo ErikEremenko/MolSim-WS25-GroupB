bd88ad5e089ec8ad56a27971b02baa2665080873
Erik Eremenko <erik.eremenko@tum.de>
2025-12-05 06:54:00 +0100

    Benchmarks

--- scripts/rebuild-yaml.sh
+++ scripts/rebuild-yaml.sh
@@ -6,4 +6,9 @@ mkdir build && cd build
 cmake -DBUILD_DOC=ON -DENABLE_VTK_OUTPUT=ON -DVTK_DIR=/usr/local/vtk/lib/cmake/vtk-9.5 ..
 make -j 8
 make doc_doxygen
-./MolSim ../input/collision3.yaml benchmark info linked
+# sets CPUs to maximum available frequency
+sudo cpupower frequency-set -g performance
+# taskset -c binds the process to the specified set of CPU cores
+# Using single-threaded calculation
+sudo taskset -c 0-19 chrt -r 50 nice -n -10 \
+./MolSim ../input/collision1000.yaml benchmark info direct
