80da5f88c2743520665f921cb17d62b54aac3904
Erik Eremenko <erik.eremenko@tum.de>
2025-12-12 13:50:10 +0100

    Reverted MaxWellBoltzmannDistribution to default formula, made ParticleContainer compatible

--- include/utils/MaxwellBoltzmannDistribution.h
+++ include/utils/MaxwellBoltzmannDistribution.h
@@ -9,31 +9,26 @@
 
 #include <array>
 #include <random>
-#include <cmath>
-
-#define BOLTZMANN_CONSTANT 1.3806503e-23
 
 /**
- * Generate a random velocity vector according to the Maxwell-Boltzmann distribution, with given temperature and mass.
+ * Generate a random velocity vector according to the Maxwell-Boltzmann distribution, with a given average velocity.
  *
- * @param temp The temperature of the whole object in Kelvin
- * @param mass The mass of a single particle
+ * @param averageVelocity The average velocity of the brownian motion for the system.
  * @param dimensions Number of dimensions for which the velocity vector shall be generated. Set this to 2 or 3.
  * @return Array containing the generated velocity vector.
  */
 inline std::array<double, 3> maxwellBoltzmannDistributedVelocity(
-    double temp, double mass, size_t dimensions) {
+    double averageVelocity, size_t dimensions) {
   // we use a constant seed for repeatability.
   // random engine needs static lifetime otherwise it would be recreated for every call.
-  static std::default_random_engine randomEngine(42069);
+  static std::default_random_engine randomEngine(42);
 
   // when adding independent normally distributed values to all velocity components
   // the velocity change is maxwell boltzmann distributed
-  const double stddev = std::sqrt(BOLTZMANN_CONSTANT * temp / mass);
-  std::normal_distribution<double> normalDistribution{0, stddev};
+  std::normal_distribution<double> normalDistribution{0, 1};
   std::array<double, 3> randomVelocity{};
   for (size_t i = 0; i < dimensions; ++i) {
-    randomVelocity[i] = normalDistribution(randomEngine);
+    randomVelocity[i] = averageVelocity * normalDistribution(randomEngine);
   }
   return randomVelocity;
 }
--- src/ParticleGenerator.cpp
+++ src/ParticleGenerator.cpp
@@ -15,7 +15,7 @@ void ParticleGenerator::generateCuboid(std::array<double, 3> cx, std::array<doub
   for (int nx = 0; nx < n[0]; nx++) {
     for (int ny = 0; ny < n[1]; ny++) {
       for (int nz = 0; nz < n[2]; nz++) {
-        std::array<double, 3> temperatureVel = maxwellBoltzmannDistributedVelocity(t, m, 2);
+        std::array<double, 3> temperatureVel = maxwellBoltzmannDistributedVelocity(t, 2);
         std::array<double, 3> particleVelocity = {cv[0] + temperatureVel[0], cv[1] + temperatureVel[1], cv[2]};
         std::array<double, 3> particlePosition = {cx[0] + h * nx, cx[1] + h * ny, cx[2] + h * nz};
         particles.addParticle(particlePosition, particleVelocity, m);
@@ -34,7 +34,7 @@ void ParticleGenerator::generateDisc(std::array<double, 3> cx, std::array<double
       double px = i * h;
       double py = j * h;
       if (px * px + py * py <= radius_sq) {
-        std::array<double, 3> temperatureVel = maxwellBoltzmannDistributedVelocity(t, m, 2);
+        std::array<double, 3> temperatureVel = maxwellBoltzmannDistributedVelocity(t, 2);
         std::array<double, 3> tempv = {cv[0] + temperatureVel[0], cv[1] + temperatureVel[1], cv[2] + temperatureVel[2]};
         std::array<double, 3> tempx = {cx[0] + px, cx[1] + py, cx[2]};
         particles.addParticle(tempx, tempv, m);
