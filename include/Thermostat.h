b30ea3c196fa7662517210615113d2e4abb14f65
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2026-01-05 16:18:49 +0300

    Fix documentation in Thermostat.h and make calculateCurrentTemperature() public

--- include/Thermostat.h
+++ include/Thermostat.h
@@ -28,8 +28,6 @@ private:
    * @brief Maximal absolute temperature change allowed for one application of the thermostat.
    */
   double tempDelta;
-
-  double calculateCurrentTemperature();
 public:
   ~Thermostat();
   Thermostat(ParticleContainer& particles, int nThermostat, double tempTarget, double tempDelta = std::numeric_limits<double>::infinity());
@@ -49,9 +47,15 @@ public:
    */
   void setTemperature(double tempNew);
   /**
-   * @brief Gradually scales the . This function should be called every @nThermoStat iterations.
+   * @brief A single application of the thermostat using gradual temperature scaling.
+   * This function should be called every @nThermoStat iterations.
    */
   void updateTemperature();  // TODO: Make this function inline
+  /**
+   * Calculates the current temperature of the system by calculating the kinetic energy of the particles.
+   * @return The current temperature of the system
+   */
+  double calculateCurrentTemperature();
 
   /**
    * @return The update frequency of the thermostat, see @ref nThermostat.
