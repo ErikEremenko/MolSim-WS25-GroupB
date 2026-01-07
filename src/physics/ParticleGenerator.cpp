39141ca64cb86165ab8a85a43c98a6f410219a9c
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2026-01-07 17:19:41 +0300

    Switch to lazy generation for particles in ParticleGenerator

--- src/physics/ParticleGenerator.cpp
+++ src/physics/ParticleGenerator.cpp
@@ -10,39 +10,81 @@
 #endif  // SPDLOG_ACTIVE_LEVEL
 #include <spdlog/spdlog.h>
 
-ParticleGenerator::ParticleGenerator(ParticleContainer& particles) : particles(particles) {}
+ParticleGenerator::ParticleGenerator() = default;
 ParticleGenerator::~ParticleGenerator() = default;
 
-void ParticleGenerator::generateCuboid(std::array<double, 3> cx, std::array<double, 3> cv, std::array<int, 3> n,
-                                       double h, double m, double t, double sigma, double epsilon) const {
-  for (int nx = 0; nx < n[0]; nx++) {
-    for (int ny = 0; ny < n[1]; ny++) {
-      for (int nz = 0; nz < n[2]; nz++) {
-        std::array<double, 3> temperatureVel = maxwellBoltzmannDistributedVelocity(std::sqrt(t / m), 3);
-        std::array<double, 3> particleVelocity = {cv[0] + temperatureVel[0], cv[1] + temperatureVel[1],
-                                                  cv[2] + temperatureVel[2]};
-        std::array<double, 3> particlePosition = {cx[0] + h * nx, cx[1] + h * ny, cx[2] + h * nz};
-        particles.addParticle(particlePosition, particleVelocity, m, sigma, epsilon);
+void ParticleGenerator::generateCuboid(ParticleContainer& container, const CuboidOrder& order) {
+  for (int nx = 0; nx < order.dim[0]; nx++) {
+    for (int ny = 0; ny < order.dim[1]; ny++) {
+      for (int nz = 0; nz < order.dim[2]; nz++) {
+        std::array<double, 3> temperatureVel = maxwellBoltzmannDistributedVelocity(std::sqrt(order.temp / order.m), 3);
+        std::array<double, 3> particleVelocity = {order.vel[0] + temperatureVel[0], order.vel[1] + temperatureVel[1],
+                                                  order.vel[2] + temperatureVel[2]};
+        std::array<double, 3> particlePosition = {order.pos[0] + order.h * nx, order.pos[1] + order.h * ny, order.pos[2] + order.h * nz};
+        container.addParticle(particlePosition, particleVelocity, order.m, order.sigma, order.epsilon);
       }
     }
   }
 }
 
-void ParticleGenerator::generateDisc(std::array<double, 3> cx, std::array<double, 3> cv, int rn, double h, double m,
-                                     double t, double sigma, double epsilon) const {
+void ParticleGenerator::generateDisc(ParticleContainer& container, const DiscOrder& order) {
 
-  double radius_sq = rn * rn * h * h;
+  double radius_sq = order.r_n * order.r_n * order.h * order.h;
 
-  for (int i = -rn; i <= rn; ++i) {
-    for (int j = -rn; j <= rn; ++j) {
-      double px = i * h;
-      double py = j * h;
+  for (int i = -order.r_n; i <= order.r_n; ++i) {
+    for (int j = -order.r_n; j <= order.r_n; ++j) {
+      double px = i * order.h;
+      double py = j * order.h;
       if (px * px + py * py <= radius_sq) {
-        std::array<double, 3> temperatureVel = maxwellBoltzmannDistributedVelocity(t, 3);
-        std::array<double, 3> tempv = {cv[0] + temperatureVel[0], cv[1] + temperatureVel[1], cv[2] + temperatureVel[2]};
-        std::array<double, 3> tempx = {cx[0] + px, cx[1] + py, cx[2]};
-        particles.addParticle(tempx, tempv, m, sigma, epsilon);
+        std::array<double, 3> temperatureVel = maxwellBoltzmannDistributedVelocity(order.temp, 3);
+        std::array<double, 3> tempv = {order.vel[0] + temperatureVel[0], order.vel[1] + temperatureVel[1], order.vel[2] + temperatureVel[2]};
+        std::array<double, 3> tempx = {order.center[0] + px, order.center[1] + py, order.center[2]};
+        container.addParticle(tempx, tempv, order.m, order.sigma, order.epsilon);
       }
     }
   }
+}
+
+void ParticleGenerator::queueCuboid(std::array<double, 3> position, std::array<double, 3> velocity,
+                                    std::array<int, 3> dimensions, double mesh_width, double mass, double temperature,
+                                    double sigma, double epsilon) {
+  cuboidOrders.push_back({position, velocity, dimensions, mesh_width, mass, temperature, sigma, epsilon});
+}
+
+void ParticleGenerator::queueDisc(std::array<double, 3> cx, std::array<double, 3> cv, int rn, double h, double m, double t,
+                 double sigma, double epsilon) {
+  discOrders.push_back({cx, cv, rn, h, m, t, sigma, epsilon});
+}
+
+void ParticleGenerator::queueParticle(std::array<double, 3> position, std::array<double, 3> velocity, double mass,
+                                      std::array<double, 3> force, std::array<double, 3> old_force, int type,
+                                      double sigma, double epsilon) {
+  explicitOrders.push_back({position, velocity, mass, force, old_force, type, sigma, epsilon});
+}
+
+void ParticleGenerator::generate(ParticleContainer& container) {
+  SPDLOG_INFO("Executing particle generation orders...");
+
+  // Generate cuboids
+  for (const auto& order : cuboidOrders) {
+    generateCuboid(container, order);
+  }
+  cuboidOrders.clear();
+  cuboidOrders.shrink_to_fit();
+
+  // Generate discs
+  for (const auto& order : discOrders) {
+    generateDisc(container, order);
+  }
+  discOrders.clear();
+  discOrders.shrink_to_fit();
+
+  // Generate explicit particles
+  for (const auto& p : explicitOrders) {
+    container.addParticle(p.x, p.v, p.m, p.f, p.oldF, p.type, p.sigma, p.epsilon);
+  }
+  explicitOrders.clear();
+  explicitOrders.shrink_to_fit();
+
+  SPDLOG_INFO("Particle generation complete.");
 }
