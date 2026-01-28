27ffe796adfedd560e73357899a5ef7bd8c091cf
Berk Erdemoglu <berkerdemoglu1120@gmail.com>
2026-01-28 17:30:35 +0100

    Added displacement resetting to thermodynamics statistics

--- src/physics/thermodynamics/MSDCalculator.cpp
+++ src/physics/thermodynamics/MSDCalculator.cpp
@@ -7,9 +7,9 @@ MSDCalculator::MSDCalculator(ParticleContainer& particles) : particles(particles
 double MSDCalculator::calculateDiffusion() {
   double displacement_squared_sum = 0.0;
 
-  for (const auto& p : particles) {
+  for (auto& p : particles) {
     displacement_squared_sum += ArrayUtils::squaredL2Norm(p.getDisplacement());
-    // TODO: Add displacement resetting
+    p.resetDisplacement();
   }
 
   return displacement_squared_sum / particles.size();
