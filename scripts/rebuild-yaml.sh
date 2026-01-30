39c6d2a754e3cba334a4e5e1ff1ce70d0f03917e
Erik Eremenko <erik.eremenko@tum.de>
2026-01-30 06:47:49 +0100

    Added contest setup

--- scripts/rebuild-yaml.sh
+++ scripts/rebuild-yaml.sh
@@ -11,6 +11,6 @@ sudo cpupower frequency-set -g performance
 # taskset -c binds the process to the specified set of CPU cores
 # Using single-threaded calculation
 sudo taskset -c 19 chrt -r 50 nice -n -10 \
-./MolSim ../input/profiling.yaml benchmark info linked P:ON S:TASKBASED
+./MolSim ../input/argon_equilibrate_smoothed.yaml file info linked
 # reset the ownership of all files in build to the current user
 cd .. && sudo chown -R "$USER":"$USER" build/
